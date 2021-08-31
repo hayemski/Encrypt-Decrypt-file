6d8bf543a9a9451e21c8cd7d5a0d23c0a4fae4b4
hayemski <hayemski@gmail.com>
2021-08-31 19:40:25 +0200

    Practice Encrypt

---
+++ EncryptDecrypt/web_forms/WebForm1.aspx
@@ -0,0 +1,84 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="EncryptDecrypt.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Enkripcija i dekripcija</title>
+
+    <style>
+        .m-centar { margin: 0 auto; }
+        .w1000 { width: 1000px; }
+        h1 { font-size: 1.5em; font-weight: bold; }
+        .row { width: 100%; position: relative; /* width: 45em; */ }
+            .row:after { clear: both; }
+
+        .button1 { display: inline-block; padding: 0.35em 1.2em; border: 0.1em solid #000000; margin: 0 0.3em 0.3em 0; border-radius: 0.12em; box-sizing: border-box; text-decoration: none; font-family: 'Roboto',sans-serif; font-weight: 300; color: #000000; text-align: center; transition: all 0.2s; }
+            .button1:hover { color: #FFFFFF; background-color: #000000; }
+        #kljuc { height: 26px; }
+        #btnFileupload { position: absolute; z-index: 10000000000; left: 48em; top: -1px; }
+    </style>
+
+    <link rel="stylesheet" type="text/css" href="..\css\reset.css" />
+    <link rel="stylesheet" type="text/css" href="..\css\index.css" />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400&display=swap" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+</head>
+<body>
+    <form id="form1" class="m-centar w1000 centar pad-top-2 " runat="server">
+        <h1>Enkripcija i dekripcija (AES/Rijndael)
+            <br />
+            Tehnike zaštite računarskih mreža</h1>
+        <div class="break">
+
+            <div class="pad-top-3">
+                <asp:DropDownList ID="DropDownList1" runat="server">
+                    <asp:ListItem Text="AES" Value="1"></asp:ListItem>
+                    <asp:ListItem Text="Triple DES (3DES)" Value="2"></asp:ListItem>
+                </asp:DropDownList>
+            </div>
+
+            <div class="pad-top-3">
+                <div class="fileupload-group ">
+                    <button type="button" runat="server" id="btnFileupload" class="btnFileupload  button1">
+                        Odabir fajla <i class="fa fa-folder-open"></i>
+                        <asp:FileUpload ID="FileUpload1" runat="server" AllowMultiple="True" />
+                    </button>
+                    <div>
+                        <input type="text" runat="server" id="txtFileuploadName" class="fileupload-name bordura" readonly="readonly" />
+                    </div>
+                </div>
+            </div>
+
+            <div class="row pad-top-2">
+                <label>Unesite vrednost ključa: </label>
+                <input id="kljuc" name="kljuc" type="Text" runat="server" />
+            </div>
+            <div class="row pad-top-2 ">
+                <asp:Button ID="btnEncrypt" CssClass="button1" Text="Enkriptuj Fajl" runat="server" OnClick="EncryptFile_Click" />
+                <asp:Button ID="btnDecrypt" CssClass="button1" Text="Dekriptuj Fajl" runat="server" OnClick="DecryptFile_Click" />
+            </div>
+        </div>
+    </form>
+
+    <script>
+        $('input[id="<%= FileUpload1.ClientID %>"]').change(function () {
+            var names = [];
+            var length = $(this).get(0).files.length;
+            for (var i = 0; i < $(this).get(0).files.length; ++i) {
+                names.push($(this).get(0).files[i].name);
+            }
+            if (length > 2) {
+                $("#<%=txtFileuploadName.ClientID%>").attr("value", length + " selected files");
+            }
+            else {
+                $("#<%=txtFileuploadName.ClientID%>").attr("value", names);
+            }
+        });
+
+
+    </script>
+</body>
+</html>
---
+++ EncryptDecrypt/web_forms/WebForm1.aspx.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Security.Cryptography;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+
+namespace EncryptDecrypt
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string kljuc = "";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //public static byte[] EncryptDES(byte[] plain, String key)
+        //{
+        //    byte[] keyArray = SoapHexBinary.Parse(key).Value;
+
+        //    TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+        //    tdes.Key = keyArray;
+        //    tdes.Mode = CipherMode.CBC;
+        //    tdes.Padding = PaddingMode.None;
+        //    tdes.IV = new byte[8];
+
+        //    ICryptoTransform cTransform = tdes.CreateEncryptor();
+        //    byte[] resultArray = cTransform.TransformFinalBlock(plain, 0, plain.Length);
+        //    tdes.Clear();
+
+        //    return resultArray;
+        //}
+
+
+        //public static byte[] DecryptDES(byte[] cipher, String key)
+        //{
+        //    byte[] keyArray = SoapHexBinary.Parse(key).Value;
+
+        //    TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+        //    tdes.Key = keyArray;
+        //    tdes.Mode = CipherMode.CBC;
+        //    tdes.Padding = PaddingMode.None;
+        //    tdes.IV = new byte[8];
+
+        //    ICryptoTransform cTransform = tdes.CreateDecryptor();
+        //    byte[] resultArray = cTransform.TransformFinalBlock(cipher, 0, cipher.Length);
+        //    tdes.Clear();
+
+        //    return resultArray;
+        //}
+
+
+
+
+
+
+        protected void EncryptFile_Click(object sender, EventArgs e)
+        {
+            //Get the Input File Name and Extension.
+            string fileName = Path.GetFileNameWithoutExtension(FileUpload1.PostedFile.FileName);
+            string fileExtension = Path.GetExtension(FileUpload1.PostedFile.FileName);
+
+            //Build the File Path for the original (input) and the encrypted (output) file.
+            string input = Server.MapPath("~/Files/") + fileName + fileExtension;
+            string output = Server.MapPath("~/Files/") + fileName + "_Encrypted" + fileExtension;
+            
+            //Save the Input File, Encrypt it and save the encrypted file in output path.
+            FileUpload1.SaveAs(input);
+            kljuc = String.Format("{0}", Request.Form["kljuc"]);
+
+            if (DropDownList1.SelectedItem.Value == "1")
+            {
+                this.EncryptAES(input, output, kljuc);
+            }
+            else
+            {
+                Init3DES(kljuc);
+                EncryptFile3DES(input, output);
+            }
+
+            //Download the Encrypted File.
+            Response.ContentType = FileUpload1.PostedFile.ContentType;
+            Response.Clear();
+            Response.AppendHeader("Content-Disposition", "attachment; filename=" + Path.GetFileName(output));
+            Response.WriteFile(output);
+            Response.Flush();
+
+            //Delete the original (input) and the encrypted (output) file.
+            File.Delete(input);
+            File.Delete(output);
+
+            Response.End();
+        }
+
+        protected void DecryptFile_Click(object sender, EventArgs e)
+        {
+            //Get the Input File Name and Extension
+            string fileName = Path.GetFileNameWithoutExtension(FileUpload1.PostedFile.FileName);
+            string fileExtension = Path.GetExtension(FileUpload1.PostedFile.FileName);
+
+            //Build the File Path for the original (input) and the decrypted (output) file
+            string input = Server.MapPath("~/Files/") + fileName + fileExtension;
+            string output = Server.MapPath("~/Files/") + fileName + "_Decrypted" + fileExtension;
+
+            //Save the Input File, Decrypt it and save the decrypted file in output path.
+            FileUpload1.SaveAs(input);
+            kljuc = String.Format("{0}", Request.Form["kljuc"]);
+
+
+            if (DropDownList1.SelectedItem.Value == "1")
+            {
+                this.DecryptAES(input, output, kljuc);
+            }
+            else
+            {
+                Init3DES(kljuc);
+                DecryptFile3DES(input, output);
+            }
+
+            //Download the Decrypted File.
+            Response.Clear();
+            Response.ContentType = FileUpload1.PostedFile.ContentType;
+            Response.AppendHeader("Content-Disposition", "attachment; filename=" + Path.GetFileName(output));
+            Response.WriteFile(output);
+            Response.Flush();
+
+            //Delete the original (input) and the decrypted (output) file.
+            File.Delete(input);
+            File.Delete(output);
+
+            Response.End();
+        }
+
+        private TripleDESCryptoServiceProvider des = new TripleDESCryptoServiceProvider();
+        public void Init3DES(string key)
+        {
+            des.Key = UTF8Encoding.UTF8.GetBytes(key);
+            des.Mode = CipherMode.ECB; //Electronic Code Book
+            des.Padding = PaddingMode.PKCS7;
+        }
+
+        public void EncryptFile3DES(string input, string output)
+        {
+            byte[] Bytes = File.ReadAllBytes(input);
+            byte[] eBytes = des.CreateEncryptor().TransformFinalBlock(Bytes, 0, Bytes.Length);
+            File.WriteAllBytes(output, eBytes);
+        }
+
+        public void DecryptFile3DES(string filepath, string output)
+        {
+            byte[] Bytes = File.ReadAllBytes(filepath);
+            byte[] dBytes = des.CreateDecryptor().TransformFinalBlock(Bytes, 0, Bytes.Length);
+            File.WriteAllBytes(output, dBytes);
+        }
+
+        private void EncryptAES(string inputFilePath, string outputfilePath, string kljuc)
+        {
+            string EncryptionKey = kljuc;
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (FileStream fsOutput = new FileStream(outputfilePath, FileMode.Create))
+                {
+                    using (CryptoStream cs = new CryptoStream(fsOutput, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        using (FileStream fsInput = new FileStream(inputFilePath, FileMode.Open))
+                        {
+                            int data;
+                            while ((data = fsInput.ReadByte()) != -1)
+                            {
+                                cs.WriteByte((byte)data);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void DecryptAES(string inputFilePath, string outputfilePath, string kljuc)
+        {
+            string EncryptionKey = kljuc;
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (FileStream fsInput = new FileStream(inputFilePath, FileMode.Open))
+                {
+                    using (CryptoStream cs = new CryptoStream(fsInput, encryptor.CreateDecryptor(), CryptoStreamMode.Read))
+                    {
+                        using (FileStream fsOutput = new FileStream(outputfilePath, FileMode.Create))
+                        {
+                            int data;
+                            while ((data = cs.ReadByte()) != -1)
+                            {
+                                fsOutput.WriteByte((byte)data);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        
+    }
+
+
+}
